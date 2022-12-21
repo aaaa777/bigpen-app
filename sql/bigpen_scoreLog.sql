6f64d42d931e2995544985bd2d8f068382c49e0e
akkun0830 <120615931+akkun0830@users.noreply.github.com>
2022-12-22 07:35:44 +0900

    スコアの毎回保存に対応するための変更（日時の追加、主キーの追加）

--- sql/bigpen_scoreLog.sql
+++ sql/bigpen_scoreLog.sql
@@ -1,12 +1,11 @@
 CREATE TABLE bigpen_scoreLog (
-    user_id int,
-    category_id int,
-    score int,
-    PRIMARY KEY(user_id, category_id),
+    score_id int PRIMARY KEY,
+    user_id int NOT NULL,
+    category_id int NOT NULL,
+    score int NOT NULL CHECK(score >= 0),
+    date_finish date NOT NULL DEFAULT now(2),
     FOREIGN KEY(user_id)
         REFERENCES bigpen_users(user_id),
     FOREIGN KEY(category_id)
-        REFERENCES bigpen_quizCategory(category_id),
-    CONSTRAINT score_check
-        CHECK(score >= 0)
+        REFERENCES bigpen_quizCategory(category_id)
 );
