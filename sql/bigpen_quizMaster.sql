93ff544ace4f4a1ea158e096dc2c0ca2e3280aed
akkun0830 <120615931+akkun0830@users.noreply.github.com>
2022-12-22 11:01:35 +0900

    正解択の番号、解説文、出題年度のカラムを追加

--- sql/bigpen_quizMaster.sql
+++ sql/bigpen_quizMaster.sql
@@ -2,6 +2,9 @@ CREATE TABLE bigpen_quizMaster (
     quiz_id serial PRIMARY KEY,
     category_id int NOT NULL,
     quiz_text TEXT,
+    true_anser int,
+    anser_text TEXT,
+    quiz_year varchar(20),
     FOREIGN KEY(category_id)
         REFERENCES bigpen_quizCategory(category_id)
 );
