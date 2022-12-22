f5396e7419f29ee06c81fd3f28e665cd7ab24f8b
akkun0830 <120615931+akkun0830@users.noreply.github.com>
2022-12-22 11:27:48 +0900

    選択肢と解説のカラムを追加

--- sql/bigpen_quizMaster.sql
+++ sql/bigpen_quizMaster.sql
@@ -2,8 +2,12 @@ CREATE TABLE bigpen_quizMaster (
     quiz_id serial PRIMARY KEY,
     category_id int NOT NULL,
     quiz_text TEXT,
+    anser1_text varchar(50),
+    anser2_text varchar(50),
+    anser3_text varchar(50),
+    anser4_text varchar(50),
     true_anser int,
-    anser_text TEXT,
+    comment TEXT,
     quiz_year varchar(20),
     FOREIGN KEY(category_id)
         REFERENCES bigpen_quizCategory(category_id)
