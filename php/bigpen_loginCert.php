d146b95dc6d4339118f7a39bf15a819875b831ed
akkun0830 <120615931+akkun0830@users.noreply.github.com>
2023-01-12 10:59:51 +0900

    ログイン用外部ファイルの内容をアップロード

--- php/bigpen_loginCert.php
+++ php/bigpen_loginCert.php
@@ -1,1 +1,32 @@
-<-- -->
+<?php
+    if(isset($_SESSION["username"])){
+        session_regenerate_id(true);
+        header("Location: ./index.php");
+        exit;
+    }
+    if(isset($_POST["username"], $_POST["password"])){
+        $db = "mysql: host=localhost; dbname=bigpen; charset=utf8mb4";
+        $username = "bpuser";
+        $password = "bppassword"
+
+        $pdo = new PDO($db, $username, $password);
+
+        $stmt = $pdo -> prepare("SELECT * FROM bpusers WHERE username=:username;");
+        $stmt -> vindValue(":username", $_POST["username"], PDO::PARAM_STR);
+        $stmt -> execute();
+
+        $row = $stmt -> FETCH(PDO::FETCH_ASSOC);
+        if(isset($row["username"])){
+            if(password_verify($_POST["password"] == $row["password"])){
+                session_regenerate_id(true);
+                $_SESSION["username"] = $_POST["username"];
+                header("Location: ./index.php");
+                exit;
+            }else{
+                echo "password is wrong";
+            }
+        }else{
+            echo "username is wrong";
+        }
+    }
+?>
